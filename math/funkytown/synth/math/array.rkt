d1a6d04be0f3440d6c59b27855ddd2582fd26d41
Ben Greenman <types@ccs.neu.edu>
2015-01-05 17:47:24 -0500

    [math-funky] much smaller provide in array.rkt

--- math/funkytown/synth/math/array.rkt
+++ math/funkytown/synth/math/array.rkt
@@ -1,54 +1,18 @@
 #lang racket/base
 
-(require "private/array/array-struct.rkt"
-         "private/array/array-constructors.rkt"
-         "private/array/array-pointwise.rkt"
-         "private/array/array-indexing.rkt"
-         "private/array/array-broadcast.rkt"
-         "private/array/array-transform.rkt"
-         "private/array/array-convert.rkt"
-         "private/array/array-fold.rkt"
-         "private/array/array-unfold.rkt"
-         "private/array/array-print.rkt"
-         "private/array/array-fft.rkt"
-         "private/array/array-syntax.rkt"
-         "private/array/utils.rkt"
-         "private/array/array-comprehension.rkt"
-         "private/array/array-sequence.rkt"
-         "private/array/mutable-array.rkt"
-         "private/array/flarray-struct.rkt"
-         "private/array/flarray-pointwise.rkt"
-         "private/array/fcarray-struct.rkt"
-         "private/array/fcarray-pointwise.rkt"
-         "private/array/array-parallel.rkt"
-         )
+(require
+ (only-in "private/array/array-broadcast.rkt"     array-broadcasting)
+ (only-in "private/array/array-comprehension.rkt" for/array)
+ (only-in "private/array/array-constructors.rkt"  make-array)
+ (only-in "private/array/array-pointwise.rkt"     array-map)
+ (only-in "private/array/array-sequence.rkt"      in-array)
+ (only-in "private/array/array-struct.rkt"        array-size build-array array-strictness)
+ (only-in "private/array/array-transform.rkt"     array-append*))
 
-;; Set the custom printer to a pretty one
-(array-custom-printer print-array)
-
-(provide (all-from-out "private/array/array-transform.rkt"
-          "private/array/array-struct.rkt"
-          "private/array/array-constructors.rkt"
-          "private/array/array-pointwise.rkt"
-          "private/array/array-indexing.rkt"
-          "private/array/array-broadcast.rkt"
-          "private/array/array-convert.rkt"
-          "private/array/array-fold.rkt"
-          "private/array/array-unfold.rkt"
-          "private/array/array-print.rkt"
-          "private/array/array-syntax.rkt"
-          "private/array/array-fft.rkt"
-          "private/array/array-comprehension.rkt"
-          "private/array/array-sequence.rkt"
-          "private/array/mutable-array.rkt"
-          "private/array/flarray-struct.rkt"
-          "private/array/flarray-pointwise.rkt"
-          "private/array/fcarray-struct.rkt"
-          "private/array/fcarray-pointwise.rkt"
-          "private/array/array-parallel.rkt"
-          )
-         ;; from utils.rkt
-         Listof*
-         Vectorof*
-         Indexes
-         In-Indexes)
+(provide array-size build-array array-strictness
+         in-array
+         make-array
+         for/array
+         array-map
+         array-broadcasting
+         array-append*)
