48bc37ddefba1758adb75d9cefd3c36061ec2d18
Ben Greenman <types@ccs.neu.edu>
2015-01-05 16:38:28 -0500

    [shootout] using nat? contract in ackermann

--- shootout/ackermann.rkt
+++ shootout/ackermann.rkt
@@ -1,13 +1,18 @@
 #lang racket/base
 
 (require racket/contract)
+(define nat? natural-number/c)
 
 (define (ack m n)
-  (-> exact-nonnegative-integer? exact-nonnegative-integer? exact-nonnegative-integer?)
+  (-> nat? nat? nat?)
   (cond ((zero? m) (+ n 1))
         ((zero? n) (ack (- m 1) 1))
         (else      (ack (- m 1) (ack m (- n 1))))))
 
-(define (main)
-  (ack 3 12))
-(time (begin (main) (void)))
+;; CPU time = 24ms without contracts
+;;          = 4987ms  with contracts
+;; (time (begin (ack 3 8) (void)))
+
+;; CPU time = 5758ms without contracts
+;;          = 
+(time (begin (ack 3 12) (void)))
